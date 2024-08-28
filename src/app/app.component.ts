171783d9e05e515b78b50295343a08548f68e3e9
alan <alan@juninho>
2024-08-28 12:49:26 -0300

    Feat: removendo spartan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AvatarComponent } from './components/avatar/avatar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AvatarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
-    './src/**/*.{html,ts}',
-    './components/**/*.{html,ts}',
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
