f69d5317605bffed9f7842f51ff80c9d93fc5de6
Landev01 <alnjr0001@outlook.com>
2024-08-29 01:39:00 -0300

    feat: configurando tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'shape-blue': '#000842',
+        'primary': '#0C21C1'
+      }
+    },
   },
   plugins: [],
 }
