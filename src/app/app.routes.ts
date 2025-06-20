512e46ae1b984abdd9806262a1850e03013bf463
LanDev01 <alnjr0001@outlook.com>
2025-06-20 19:37:09 -0300

    feat: add sign-in route and integrate RouterOutlet in app component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import type { Routes } from '@angular/router';
+import { SignIn } from './pages/sign-in/sign-in';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SignIn,
+  },
+];
