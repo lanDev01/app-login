6042dd88bff0e91d28dbb7a19a541be3a4085a26
LanDev01 <alnjr0001@outlook.com>
2025-06-20 19:25:16 -0300

    feat: implement input component with password visibility toggle and unit tests

---
+++ src/app/ui/input/input.ts
@@ -0,0 +1,45 @@
+import { Component, input, signal } from '@angular/core';
+import { LucideAngularModule, Eye, EyeOff } from 'lucide-angular';
+
+@Component({
+  selector: 'app-input',
+  imports: [LucideAngularModule],
+  templateUrl: './input.html',
+})
+export class Input {
+  text = input<string>('');
+  placeholder = input<string>('');
+  type = input<'text' | 'email' | 'password'>('text');
+  forgot = input<boolean>(false);
+
+  inputValue = signal('');
+  isPasswordVisible = signal(false);
+
+  readonly eye = Eye;
+  readonly eyeOff = EyeOff;
+
+  get inputType(): string {
+    if (this.type() !== 'password') return this.type();
+    return this.isPasswordVisible() ? 'text' : 'password';
+  }
+
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.inputValue.set(input.value);
+  }
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible.update(prev => !prev);
+  }
+
+  get autocomplete(): string {
+    switch (this.type()) {
+      case 'email':
+        return 'email';
+      case 'password':
+        return 'current-password';
+      default:
+        return 'on';
+    }
+  }
+}
