38119986f471cc8a24bb875d7e0f864b61f86fb6
LanDev01 <alnjr0001@outlook.com>
2025-06-20 19:20:51 -0300

    feat: add button component with template and unit tests

---
+++ src/app/ui/button/button.ts
@@ -0,0 +1,10 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+})
+export class Button {
+  text = input<string>('');
+}
